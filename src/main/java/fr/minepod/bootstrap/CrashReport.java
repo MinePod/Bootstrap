35b6a1ca2bf908016d100154d0f6a677dbb58119
DarkShimy00 <darkshimy00@gmail.com>
2013-12-04 20:28:17 +0100

    Check if debug file exists

--- src/main/java/fr/minepod/bootstrap/CrashReport.java
+++ src/main/java/fr/minepod/bootstrap/CrashReport.java
@@ -1,16 +1,21 @@
 package fr.minepod.bootstrap;
 
 import java.awt.HeadlessException;
+import java.io.File;
 import java.io.IOException;
 
 public class CrashReport {
-	public static String DebugFilePath = Config.DebugFilePath;
-	
 	public static void SendReport(String exception, String when) {
 		try {
 			if(Config.Logger != null)
 				Config.Logger.severe(exception);
-			javax.swing.JOptionPane.showMessageDialog(null, exception + "\n" +  Langage.WHEN.toString() + when + "\n\n\n" + Langage.DEBUGINFORMATIONS.toString() + ":\n\n" + fr.minepod.Utils.Files.ReadFile(DebugFilePath), Langage.ERROR.toString(), javax.swing.JOptionPane.ERROR_MESSAGE);
+			
+			String debugMsg = "";
+			
+			if(new File(Config.DebugFilePath).exists())
+				debugMsg = fr.minepod.Utils.Files.ReadFile(Config.DebugFilePath);
+			
+			javax.swing.JOptionPane.showMessageDialog(null, exception + "\n" +  Langage.WHEN.toString() + when + "\n\n\n" + Langage.DEBUGINFORMATIONS.toString() + ":\n\n" + debugMsg, Langage.ERROR.toString(), javax.swing.JOptionPane.ERROR_MESSAGE);
 		} catch (HeadlessException e) {
 			if(Config.Logger != null)
 				Config.Logger.severe(e.toString());
